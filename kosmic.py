69e89265b0a515c372f51255c2b62f0469419c68
Rick Elrod <rick@elrod.me>
2021-02-25 22:15:16 -0600

    First attempt at GHA builds
    Signed-off-by: Rick Elrod <rick@elrod.me>

---
+++ kosmic.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import datetime
+import twitter
+import os
+import yaml
+
+api = twitter.Api(
+    consumer_key=os.environ.get('CONSUMER_KEY'),
+    consumer_secret=os.environ.get('CONSUMER_SECRET'),
+    access_token_key=os.environ.get('ACCESS_KEY'),
+    access_token_secret=os.environ.get('ACCESS_SECRET'))
+
+# Load in the cached/previously stored tweets
+cache = yaml.safe_load(open('data.yml'))
+latest_id = cache[-1]['id']
+
+tl = api.GetUserTimeline(
+    screen_name='kosmicd12',
+    count=200,
+    exclude_replies=True,
+    include_rts=False,
+    since_id=latest_id
+)
+
+for tweet in tl[::-1]:
+    if tweet.quoted_status is not None:
+        q = tweet.quoted_status
+        if q.id == latest_id:
+            new = {'id': tweet.id, 'text': tweet.text}
+            print(new)
+            cache.append(new)
+            latest_id = tweet.id
+
+with open('data.yml', 'w') as f:
+    f.write(yaml.dump(cache))
+
+out = []
+new_para = False
+for ele in cache[::-1]:
+    word = ele['text'].rsplit(' ', 1)[0]
+    link = '<a href="https://twitter.com/Kosmicd12/status/{0}">{1}</a>'.format(
+        ele['id'],
+        word)
+    if new_para:
+        link = '</p><p>{0}'.format(link)
+    new_para = word.endswith('.')
+    out.append(link)
+
+with open('index.html', 'w') as f:
+    f.write('''<!doctype html>
+<html>
+  <head>
+    <title>Kosmic's Daily New Year Tweet</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@0;1&display=swap" rel="stylesheet">
+    <style>
+      body { margin: 0 auto; width: 600px; }
+      p { font-family: 'Playfair Display', serif; font-size: 2em; }
+      p.attr { text-align: right; font-size: 1em; }
+      p.info { font-size: 0.6em; color: #666; text-align: center; }
+      a { text-decoration: none; color: #000; }
+    </style>
+  </head>
+  <body>
+    <p>&hellip; %s</p>
+    <p class="attr">- @Kosmicd12</p>
+    <p class="info">
+      Checks for updates every hour.
+      <a href="https://github.com/relrod/kosmic-new-year">open source</a>.
+    </p>
+    <p class="info">
+      Last update: %s UTC
+    </p>
+  </body>
+</html>
+''' % (' '.join(out), datetime.datetime.utcnow()))
